00b51308741c749063b08a7c60762607237bff54
Andrew <andrewdircks@me.com>
2020-04-15 23:15:30 -0400

    Include PM .csv data

--- src/visualize_distribution_index.py
+++ src/visualize_distribution_index.py
@@ -8,11 +8,11 @@ parents = [(3, 3), (6, 3), (4.5, 6)]
 eta = 0.1
 zeta = 0.1
 probability = 1
-distributionIndex = [100, 50, 20, 15, 10, 0]
+distributionIndex = [90]
 bounds = [(0, 10), (0, 10)]
-num_samples = 100
+num_samples = 1000
 
-fig, axs = plt.subplots(1, len(distributionIndex))
+# fig, axs = plt.subplots(1, len(distributionIndex))
 
 children = []
 
@@ -22,12 +22,14 @@ for i in range(num_samples):
 
 for j in range(len(distributionIndex)):
     di = distributionIndex[j]
-    axs[j].axis([0, 10, 0, 10])
-    title = "DI = " + str(di)
-    axs[j].set(aspect='equal')
-    axs[j].set_title(title)
+    # axs[j].axis([0, 10, 0, 10])
+    # title = "DI = " + str(di)
+    # axs[j].set(aspect='equal')
+    # axs[j].set_title(title)
     for i in range(num_samples):
         sol = pm.pm(children[i], bounds, probability, di)
-        axs[j].scatter(sol[0], sol[1], c='red', s=5)
+        # axs[j].scatter(sol[0], sol[1], c='red', s=5)
+        s = str(sol[0]) + "," + str(sol[1])
+        print(s)
 
-plt.show()
+# plt.show()
